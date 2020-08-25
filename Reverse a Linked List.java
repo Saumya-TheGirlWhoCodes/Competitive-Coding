174339dd51d2ca5c61ab098cb565d0a3957617dd
Saumya Srivastava <51478376+Saumya-TheGirlWhoCodes@users.noreply.github.com>
2020-08-25 10:39:17 +0530

    Update Reverse a Linked List.java

--- Reverse a Linked List.java
+++ Reverse a Linked List.java
@@ -1,3 +1,14 @@
+/*
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+*/
+
+/* THIS IS AN ITERATIVE SOLUTION */
+
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode curr=head;
