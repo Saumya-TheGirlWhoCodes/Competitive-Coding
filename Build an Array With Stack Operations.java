07487cc8cad65666a2665e5d5b2a9f8d2f6df0c6
Saumya Srivastava <51478376+Saumya-TheGirlWhoCodes@users.noreply.github.com>
2020-09-16 12:49:13 +0530

    Stack operations

--- Build an Array With Stack Operations.java
+++ Build an Array With Stack Operations.java
@@ -13,7 +13,6 @@ Return the operations to build the target array.
 You are guaranteed that the answer is unique.
 
 Example 1:
-
 Input: target = [1,3], n = 3
 Output: ["Push","Push","Pop","Push"]
 Explanation: 
@@ -22,18 +21,15 @@ Read number 2 and automatically push in the array then Pop it -> [1]
 Read number 3 and automatically push in the array -> [1,3]
 
 Example 2:
-
 Input: target = [1,2,3], n = 3
 Output: ["Push","Push","Push"]
 
 Example 3:
-
 Input: target = [1,2], n = 4
 Output: ["Push","Push"]
 Explanation: You only need to read the first 2 numbers and stop.
 
 Example 4:
-
 Input: target = [2,3,4], n = 4
 Output: ["Push","Pop","Push","Push","Push"]
  
