88e362762d30807887eaa72ad6c6ca182e7fd690
Saumya Srivastava <51478376+Saumya-TheGirlWhoCodes@users.noreply.github.com>
2020-09-20 20:48:35 +0530

    Handling multiple datatypes using a single method

---
+++ Generics.java
@@ -0,0 +1,33 @@
+/*
+
+Generic methods are a very efficient way to handle multiple datatypes using a single method. This problem will test your knowledge on Java Generic methods.
+Let's say you have an integer array and a string array. You have to write a single method printArray that can print all the elements of both arrays. The method should be able to accept both integer arrays or string arrays.
+
+You are given code in the editor. Complete the code so that it prints the following lines:
+
+1
+2
+3
+Hello
+World
+
+Do not use method overloading because your answer will not be accepted.
+
+*/
+
+class Printer {
+    public <T> void printArray(T[] array) 
+    {
+        for (T item : array) 
+        {
+            System.out.println(item);
+        }
+    }
+}
+
+/*
+
+Approach-
+The array of is of a generic type T. For each element in the array, we print it to the console.
+
+*/
