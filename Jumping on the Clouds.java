70572378a16e5dc064f850431310e159088b1b0b
Saumya Srivastava <51478376+Saumya-TheGirlWhoCodes@users.noreply.github.com>
2020-09-21 20:07:51 +0530

    Update Jumping on the Clouds.java

--- Jumping on the Clouds.java
+++ Jumping on the Clouds.java
@@ -1,9 +1,7 @@
 /*
 
 Emma is playing a new mobile game involving n clouds numbered from 0 to n - 1. A player initially starts out on cloud c0, and they must jump to cloud cn-1. In each step, she can jump from any cloud  to cloud  or cloud .
-
 There are two types of clouds, ordinary clouds and thunderclouds. The game ends if Emma jumps onto a thundercloud, but if she reaches the last cloud (i.e., cn-1), she wins the game!
-
 Can you find the minimum number of jumps Emma must make to win the game? It is guaranteed that clouds c0 and cn-1 are ordinary-clouds and it is always possible to win the game.
 
 Input Format:
