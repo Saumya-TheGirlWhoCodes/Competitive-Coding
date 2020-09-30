19816ef387f748b3563456649519bd615c5ff96e
Saumya Srivastava <51478376+Saumya-TheGirlWhoCodes@users.noreply.github.com>
2020-09-30 19:03:09 +0530

    Check if Regex pattern is Valid or not

--- Pattern Syntax Checker.java
+++ Pattern Syntax Checker.java
@@ -7,15 +7,12 @@ In this problem, you are given a pattern. You have to check whether the syntax o
 Note: In this problem, a regex is only valid if you can compile it using the Pattern.compile method.
 
 Input Format
-
 The first line of input contains an integer N, denoting the number of test cases. The next N lines contain a string of any printable characters representing the pattern of a regex.
 
 Output Format
-
 For each test case, print Valid if the syntax of the given pattern is correct. Otherwise, print Invalid. Do not print the quotes.
 
 Sample Input
-
 3
 ([A-Z])(.+)
 [AZ[a-z](a-z)
